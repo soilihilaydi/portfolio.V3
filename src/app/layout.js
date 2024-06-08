17036e7173475dbbe0e6b130416570a6c49eb975
soilihi laydi <soilihi.laydi@gmail.com>
2024-06-08 16:45:05 +0200

    mise en place de la navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,11 +15,11 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
           <div className="h-24">
-           <Navbar />
+            <Navbar />
           </div>
-           <div className="h-[calc(100vh - 6rem)]">{children}</div>
-          </div>  
-        </body>
+          <div className="h-[calc(100vh - 6rem)]">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
