9a39c9e6bad133b7a1ba453bf6b290800a7cdcbe
soilihi laydi <soilihi.laydi@gmail.com>
2024-06-26 17:23:23 +0200

    creation homepage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // Ajoutez des configurations personnalisées ici 
+    return config;
+  },
+};
 
 export default nextConfig;
