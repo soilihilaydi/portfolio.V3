6f7156130b10565f543ba8b993734100fed9d875
soilihi laydi <soilihi.laydi@gmail.com>
2024-06-30 12:56:38 +0200

    creation transition

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+import TransitionProvider from "@/components/transitionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +18,8 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/favicon.ico" />  
       </head>
       <body className={inter.className}>
-        <div className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100">
-          <div className="h-24">
-            <Navbar />
-          </div>
-          <div className="h-[calc(100vh-6rem)]">{children}</div>
-        </div>
+        
+      <TransitionProvider>{children}</TransitionProvider>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,18 @@
+"use client";
+
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 
 const Homepage = () => {
 
   return (
+    <motion.div
+    className="h-full"
+    initial={{ y: "-200vh" }}
+    animate={{ y: "0%" }}
+    transition={{ duration: 1 }}
+  >
 
     <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
 
@@ -38,6 +47,7 @@ const Homepage = () => {
             </div>
         </div>
     </div>
+    </motion.div>
   );
 };
 
